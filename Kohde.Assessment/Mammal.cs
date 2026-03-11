4ce0e58297d0186705989d646e01dac3c3450bd2
MuazAbdool <mrawenxa@gmail.com>
2026-03-11 22:38:55 +0200

    Add IEntity interface and Mammal abstract class to define common properties and methods for mammals

---
+++ Kohde.Assessment/Mammal.cs
@@ -0,0 +1,13 @@
+﻿namespace Kohde.Assessment
+{
+    public abstract class Mammal : IEntity
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public virtual string GetDetails()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
